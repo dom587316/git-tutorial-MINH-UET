5bea2c7364026c34c8a104add46ac94fa0111708
Đỗ Hoàng Minh <157335790+dom587316@users.noreply.github.com>
2025-03-18 18:35:20 +0700

    Update Display.java

--- src/main/java/Display.java
+++ src/main/java/Display.java
@@ -1,5 +1,5 @@
 public class Display {
     public static void main(String[]args) {
-        System.out.println(" test branch thay đổi trên server");
+        System.out.println(" test branch thay đổi trên server lần 2");
     }
 }
